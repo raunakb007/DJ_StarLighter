bcaf3a2c1a2cb1e5cc219924b4e0035d9fb7a8a6
Juliana <mjulie.quintero@mail.utoronto.ca>
2019-08-15 20:29:13 -0400

    "added descriptions "

---
+++ SongsToYoutube.py
@@ -0,0 +1,2 @@
+"""given a list of songs, find both the original song + the acapela version as a dictionary ie
+{{original_songs: {name : link}, {name2:link} ...}, {acapela_songs: {name: link}, {name: link}}}"""
