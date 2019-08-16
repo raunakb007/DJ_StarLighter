41d86d36a868e18ffe694e2663b446cc4d8eccf9
Juliana <mjulie.quintero@mail.utoronto.ca>
2019-08-15 20:27:11 -0400

    "added descriptions "

---
+++ PlyalistReader.py
@@ -0,0 +1,2 @@
+"""Given the song and artist generate 1000 of playlist list with the song and artist within
+[ one with song, one with the artist ] and return a list of list of these songs"""
---
+++ Slack_interface.py
@@ -0,0 +1,1 @@
+"""generate a bot that will be able to accept a user's song + artist and return it to our platform"""
